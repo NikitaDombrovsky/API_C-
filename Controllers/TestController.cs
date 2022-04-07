ea8b10324f365f2473635297fb10a1eaebce4fbd
NikitaDombrovky <nikita.nikita55@mail.ru>
2022-04-07 06:14:00 +0600

    first commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Database;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        TestContext db;
+
+        public TestController(TestContext context)
+        {
+            db = context;
+            if (!db.Tests.Any())
+            {
+                db.Tests.Add(new Test
+                {
+                    ID = 1,
+                    Name = "Br",
+                    Other = "One"
+                });
+                db.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Test>>> Get()
+        {
+            return await db.Tests.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Test>> Get(int id)
+        {
+            Test test = await db.Tests.FirstOrDefaultAsync(x => x.ID == id);
+            if (test == null)
+                return NotFound();
+            return new ObjectResult(test);
+        }
+
+    }
+}
---
+++ Models/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Test
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Other { get; set; }
+    }
+}
